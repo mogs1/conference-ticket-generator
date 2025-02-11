1cd384607a9e7be332d0c80b5665153e2823b77b
Mogbo Akeredolu <109990935+mogs1@users.noreply.github.com>
2025-02-11 02:41:49 +0100

    Created the NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import NavBar from "./components/NavBar"
+import Home from "./pages/Home"
+import Tickets from "./pages/Tickets"
+import About from "./pages/About"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="">
-      {/* <NavBar /> */}
+    <div className="py-5">
+      <Router>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/about" element={About} />
+          <Route path="/tickets" element={Tickets} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,45 @@
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+import "./NavBar.css"
 import logo from "../assets/logo.png"
 import arrow from "../assets/line.svg"
 
 const NavBar = () => {
-  return (
-    <nav className="flex max-w-[1200px] mx-auto py-3 px-4 justify-between items-center rounded-3xl border-[#197686] bg-[#05252c50] backdrop-blur-[2px]">
-      <img src={logo} alt="Logo" />
+    const [isHovered, setIsHovered] = useState(false);
 
-      <ul className="flex items-center gap-4 text-[#B3B3B3] cursor-pointer">
-        <li>Events</li>
-        <li>My Tickets</li>
-        <li>About Project</li>
+  return (
+    <nav className="flex max-w-[1200px] mx-auto py-3 px-4 justify-between items-center 
+                    rounded-3xl border border-[#197686] bg-[#05252c40] backdrop-blur-[2px]">
+      <img 
+      src={logo} 
+      alt="Logo" />
+
+      <ul className="sm:flex items-center gap-4 text-[#B3B3B3] cursor-pointer hidden">
+        <li>
+            <NavLink to={'/'}>
+            Events
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to={'/tickets'}>
+            My Tickets
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to={'/about'}>
+            About
+            </NavLink>
+        </li>
       </ul>
 
-      <button className="bg-white text-black flex items-center py-4 px-6 gap-2 rounded-xl border-yellow-900 cursor-pointer">
-        MY TICKETS <img src={arrow} alt="Arrow" />
+      <button className="group bg-white text-black inline-flex items-center py-3 px-4 gap-2 
+                        sm:py-4 sm:px-6 sm:gap-2 rounded-xl cursor-pointer
+                        duration-200 hover:bg-[#24A0B5] hover:text-[#D9D9D9]">
+        MY TICKETS 
+        <img
+        className="transition-transform duration-200 rotate-0 group-hover:rotate-[-60deg]"
+        src={arrow} 
+         alt="Arrow" />
       </button>
     </nav>
   )
